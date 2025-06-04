b46e774b37d20799757c85f974def1cb532e0438
nimac <michael12sco@gmail.com>
2025-06-04 15:26:08 +0330

    fix

--- neshaa/settings/base.py
+++ neshaa/settings/base.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# Shared settings
 SECRET_KEY = os.getenv(
     "SECRET_KEY", "tF7EpQUgVm4os%ArQyL13I+g*V8JV&dvd11#c50#ni3vDe&bsS"
 )

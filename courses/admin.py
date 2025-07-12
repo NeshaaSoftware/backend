b780d66398ddbad3463f3fe029f9f8e46a3089e5
nimac <michael12sco@gmail.com>
2025-07-12 18:08:47 +0330

    feat: Add preview functionality for Excel registration uploads; enhance user experience with confirmation step

--- courses/admin.py
+++ courses/admin.py
@@ -3,10 +3,11 @@ from dalf.admin import DALFModelAdmin, DALFRelatedFieldAjax
 from django import forms
 from django.contrib import admin, messages
 from django.db.models import Q
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
 from django.urls import path, reverse
 from django.utils.html import format_html
 from django_jalali.admin.filters import JDateFieldListFilter
+import pandas as pd
 
 from commons.admin import DetailedLogAdminMixin, DropdownFilter
 from financials.admin import CourseTransactionInline
@@ -205,15 +206,30 @@ class RegistrationAdmin(DetailedLogAdminMixin, DALFModelAdmin):
         if request.method == "POST":
             form = RegistrationExcelUploadForm(request.POST, request.FILES)
             if form.is_valid():
-                excel_file = form.cleaned_data["excel_file"]
-                selected_course = form.cleaned_data["course"]
+                excel_file = request.FILES["excel_file"]
                 sheet_name = form.cleaned_data.get("sheet_name")
+                try:
+                    df = pd.read_excel(excel_file, sheet_name=sheet_name)
+                except Exception as e:
+                    self.message_user(request, f"Error reading Excel file: {e}", level="error")
+                    return redirect(request.path)
+                if not request.POST.get("confirm_preview"):
+                    preview_data = df.loc[:1, ["fix phone", "نام", "نام خانوادگی"]]
+                    preview_html = preview_data.to_html(index=False, classes="table table-bordered table-sm")
+                    return render(
+                        request,
+                        "admin/courses/registration/upload_registration_excel.html",
+                        {
+                            "form": form,
+                            "preview_html": preview_html,
+                            "show_confirm": True,
+                        },
+                    )
+                selected_course = form.cleaned_data["course"]
                 update_name = form.cleaned_data.get("update_name")
                 currency_to_toman_multiplier = form.cleaned_data.get("currency_to_toman_multiplier", 1000000)
                 try:
                     import hashlib
-
-                    import pandas as pd
                     from tqdm import tqdm
 
                     from commons.utils import get_national_id, get_status_from_text, normalize_phone

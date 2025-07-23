63f51329d14985264020a7a7d71057fe23d1650e
Ali Azarkasb <az@Alis-MacBook-Air.local>
2025-07-23 23:44:24 +0330

    make development default, activate i18n

--- neshaa/settings/__init__.py
+++ neshaa/settings/__init__.py
@@ -1,11 +1,10 @@
 # Dynamically load settings in the correct order
 import os
 
-DJANGO_ENV = os.getenv("DJANGO_ENV", "local")
-
+DJANGO_ENV = os.getenv("DJANGO_ENV", "development")
 if DJANGO_ENV == "production":
     from .production import *
 elif DJANGO_ENV == "development":
     from .development import *
 else:
-    from .local import *
+    from .local import * # Doesn't work
